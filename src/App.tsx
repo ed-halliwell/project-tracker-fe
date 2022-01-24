2265ce1094d84cd56145d9acbe3a760139624bc6
Ed Halliwell <edhalliwell@hotmail.com>
2022-01-24 12:04:11 +0000

    Add Chakra and fix errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { greet } from "./utils/greet";
+import { Container } from "@chakra-ui/react";
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return (
+    <Container maxW="container.xl">
+      <h1>{greet("World")}</h1>
+    </Container>
+  );
 }
 
 export default App;
